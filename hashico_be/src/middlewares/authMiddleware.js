161017291f65e20a476cb8919851caa590648545
Zakkutsu <154570753+zakkutsu@users.noreply.github.com>
2026-01-03 09:56:08 +0700

    feat: Add authentication middleware

---
+++ hashico_be/src/middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+module.exports = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+    if (!token) return res.status(401).json({ message: 'Akses ditolak, token tidak ada' });
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (err) {
+        res.status(400).json({ message: 'Token tidak valid' });
+    }
+};
