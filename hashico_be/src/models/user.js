e0bb46ffa629a8aa34a32c61dadb6b5b2e94c921
Zakkutsu <154570753+zakkutsu@users.noreply.github.com>
2026-01-03 09:53:25 +0700

    feat: Add user authentication models

---
+++ hashico_be/src/models/user.js
@@ -0,0 +1,12 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    username: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false }
+}, {
+    timestamps: true
+});
+
+module.exports = User;
