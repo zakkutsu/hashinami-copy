a6a0bb0a0f6b3f989044ecf6c3779875c1182018
Zakkutsu <154570753+zakkutsu@users.noreply.github.com>
2026-01-03 09:54:38 +0700

    feat: Add response utility helper

---
+++ hashico_be/src/utils/response.js
@@ -0,0 +1,13 @@
+const response = (statusCode, data, message, res) => {
+    res.status(statusCode).json({
+        payload: data,
+        message: message,
+        metadata: {
+            prev: "",
+            next: "",
+            current: ""
+        }
+    });
+};
+
+module.exports = response;
